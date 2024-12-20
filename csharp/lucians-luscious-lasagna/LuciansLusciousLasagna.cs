ca98f9ce35749c9d78fe3d16d8d708adad918399
Victor Cordoba <Victor.Cordoba@arup.com>
2024-12-20 18:13:56 +0000

    Exercise passed

--- csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,26 +1,7 @@
 class Lasagna
 {
-    // TODO: define the 'ExpectedMinutesInOven()' method
-    public int ExpectedMinutesInOven()
-    {
-        return 40;
-    }
-
-    // TODO: define the 'RemainingMinutesInOven()' method
-    public int RemainingMinutesInOven(int time)
-    {
-        return this.ExpectedMinutesInOven() - time;
-    }
-
-    // TODO: define the 'PreparationTimeInMinutes()' method
-    public int PreparationTimeInMinutes(int layers)
-    {
-        return 2 * layers;
-    }
-
-    // TODO: define the 'ElapsedTimeInMinutes()' method
-    public int ElapsedTimeInMinutes(int layers, int time)
-    {
-        return this.PreparationTimeInMinutes(layers) + time;
-    }
+    public int ExpectedMinutesInOven() => 40;
+    public int RemainingMinutesInOven(int minutesInOven) => ExpectedMinutesInOven() - minutesInOven;  
+    public int PreparationTimeInMinutes(int numberOfLayers) => 2 * numberOfLayers;
+    public int ElapsedTimeInMinutes(int numberOfLayers, int minutesInOven) => PreparationTimeInMinutes(numberOfLayers) + minutesInOven;
 }
