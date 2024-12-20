3f3ff1b6957f60b5eb5a2cbd01f4bf0e4bef4803
Victor Cordoba <Victor.Cordoba@arup.com>
2024-12-20 18:02:21 +0000

    First Iteration

---
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -0,0 +1,26 @@
+class Lasagna
+{
+    // TODO: define the 'ExpectedMinutesInOven()' method
+    public int ExpectedMinutesInOven()
+    {
+        return 40;
+    }
+
+    // TODO: define the 'RemainingMinutesInOven()' method
+    public int RemainingMinutesInOven(int time)
+    {
+        return this.ExpectedMinutesInOven() - time;
+    }
+
+    // TODO: define the 'PreparationTimeInMinutes()' method
+    public int PreparationTimeInMinutes(int layers)
+    {
+        return 2 * layers;
+    }
+
+    // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int layers, int time)
+    {
+        return this.PreparationTimeInMinutes(layers) + time;
+    }
+}
