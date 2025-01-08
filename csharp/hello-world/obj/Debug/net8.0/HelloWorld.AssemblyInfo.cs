2725717ca1d171739c4d29378ba17c0294daff8e
Victor Cordoba <Victor.Cordoba@arup.com>
2025-01-08 14:20:28 +0000

    Otra vez commit hello world files

--- csharp/hello-world/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ csharp/hello-world/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f43f7269c3b4930ae950688bc13b2921d244e434")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99f064f9732fb48589580aac5bb2524fc41259ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
