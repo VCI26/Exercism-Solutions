61cc95b3d7bec2b1c21cda2f6fe70bd9ad5ff234
Victor Cordoba <Victor.Cordoba@arup.com>
2025-02-11 16:11:26 +0000

    Phone number Analysis ex passed and submitted

--- csharp/phone-number-analysis/PhoneNumberAnalysis.cs
+++ csharp/phone-number-analysis/PhoneNumberAnalysis.cs
@@ -4,11 +4,31 @@ public static class PhoneNumber
 {
     public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.Analyze() method");
+        bool isNewYork = false;
+        bool isFake = false;
+        
+        string dialingCode = phoneNumber.Split('-')[0];
+        string prefixCode = phoneNumber.Split('-')[1];
+        string number = phoneNumber.Split('-')[2];
+
+        if (dialingCode == "212") { isNewYork = true;}
+        if (prefixCode == "555") { isFake = true;}
+
+        return (isNewYork, isFake, number);        
     }
 
     public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.IsFake() method");
+        return phoneNumberInfo.IsFake;
     }
 }
+
+//// name items in declaration
+//(bool success, string message) results = (true, "well done!");
+//bool mySuccess = results.success;
+//string myMessage = results.message;
+
+//// name items in creating expression
+//var results2 = (success: true, message: "well done!");
+//bool mySuccess2 = results2.success;
+//string myMessage2 = results2.message;
