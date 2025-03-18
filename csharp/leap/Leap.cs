226906cb3a17e3972b423e8bd37218b69bea0bd7
Victor Cordoba <Victor.Cordoba@arup.com>
2025-03-18 15:17:22 +0000

    Leap exercise completed

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -2,6 +2,14 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        if (year % 4 == 0)
+        {
+            return true;
+        }
+        if (year % 100 == 0 && year % 400 == 0)
+        {
+            return true;
+        }
+        return false;
     }
 }
